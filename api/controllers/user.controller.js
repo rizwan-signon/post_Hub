dcf003f6712b7b4e4ad55301dc64979693f53934
your-username <your-email>
2024-08-06 18:10:19 +0500

    add login route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 export const registerUser = async (req, res, next) => {
   const { userName, email, password } = req.body;
   try {
@@ -12,6 +13,27 @@ export const registerUser = async (req, res, next) => {
     });
     await user.save();
     res.status(200).json(user);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const logInUser = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const validUser = await User.findOne({ email });
+    const validPass = bcryptjs.compareSync(password, validUser.password);
+    if (!validUser || !validPass) throw new Error("wrong credentails");
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET, {
+      expiresIn: "3d",
+    });
+    res
+      .cookie("jwt", token, {
+        maxAge: 60 * 60 * 1 * 24 * 3 * 1000,
+        httpOnly: true,
+      })
+      .status(200)
+      .json(validUser);
   } catch (error) {
     next(error);
   }
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { logInUser, registerUser } from "../controllers/user.controller.js";
 const router = Router();
 router.post("/register", registerUser);
+router.post("/login", logInUser);
 export default router;
