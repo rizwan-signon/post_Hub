119cec87cb916a106c1ab08d43fac0d6b1129f8d
your-username <your-email>
2024-08-06 16:36:50 +0500

    add signin and register page UIs

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,7 +1,45 @@
+import { useState } from "react";
+
 const Register = () => {
+  const [formData, setFormData] = useState({});
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  console.log(formData);
   return (
-    <div>
-      <h1>register page </h1>
+    <div className="max-w-sm sm:max-w-lg mx-auto mt-16">
+      <h1 className="text-3xl sm:text-3xl font-medium text-blue-700 uppercase text-center mb-8">
+        register
+      </h1>
+      <form action="#" className=" shadow-xl max-w-4xl mx-auto px-6 py-10">
+        <div className="max-w-2xl mx-auto flex flex-col gap-5">
+          <input
+            onChange={handleChange}
+            type="text"
+            placeholder="userName"
+            id="userName"
+            className="w-full bg-gray-200 p-3 rounded-lg focus:outline-none"
+          />
+          <input
+            onChange={handleChange}
+            type="email"
+            placeholder="email"
+            id="email"
+            className="w-full bg-gray-200 p-3 rounded-lg focus:outline-none"
+          />
+          <input
+            onChange={handleChange}
+            type="password"
+            placeholder="password"
+            id="password"
+            className="w-full bg-gray-200 p-3 rounded-lg focus:outline-none"
+          />
+          <button className=" bg-blue-700 rounded-lg p-3 w-full text-white uppercase font-medium hover:opacity-85">
+            Register
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
